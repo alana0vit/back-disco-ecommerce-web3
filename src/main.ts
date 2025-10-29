d1bf04c00e85c1efc2d94693c0ab5d82184a69e3
alana0vit <alana.vit.ms@gmail.com>
2025-10-29 00:53:03 -0300

    feat: instalação e configuração do axios para testar requisições do front-end

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:8000',
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
+    // credentials: true, // cookies/sessões
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 
   app.useGlobalPipes(
