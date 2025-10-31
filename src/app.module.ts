d87f2cfed589b1e13187b71f77d73ee5299f1be2
alana0vit <alana.vit.ms@gmail.com>
2025-10-30 22:54:39 -0300

    fix: fazendo correções gerais na estrutura e sintaxe (uso do 'npm run lint')

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,21 +9,7 @@ import { EnderecoModule } from './endereco/endereco.module';
 import { CategoriaModule } from './categoria/categoria.module';
 import { PedidoModule } from './pedido/pedido.module';
 import { PagamentoModule } from './pagamento/pagamento.module';
-import { ProdutoService } from './produto/produto.service';
-import { ProdutoController } from './produto/produto.controller';
-import { CategoriaController } from './categoria/categoria.controller';
-import { ClienteController } from './cliente/cliente.controller';
-import { EnderecoController } from './endereco/endereco.controller';
-import { PagamentoController } from './pagamento/pagamento.controller';
-import { PedidoController } from './pedido/pedido.controller';
-import { CategoriaService } from './categoria/categoria.service';
-import { ClienteService } from './cliente/cliente.service';
-import { EnderecoService } from './endereco/endereco.service';
-import { PagamentoService } from './pagamento/pagamento.service';
-import { PedidoService } from './pedido/pedido.service';
 import { ItemPedidoModule } from './item-pedido/item-pedido.module';
-import { ItemPedidoController } from './item-pedido/item-pedido.controller';
-import { ItemPedidoService } from './item-pedido/item-pedido.service';
 
 @Module({
   imports: [
@@ -56,25 +42,7 @@ import { ItemPedidoService } from './item-pedido/item-pedido.service';
     PagamentoModule,
     ItemPedidoModule,
   ],
-  controllers: [
-    AppController,
-    ProdutoController,
-    CategoriaController,
-    ClienteController,
-    EnderecoController,
-    PagamentoController,
-    PedidoController,
-    ItemPedidoController,
-  ],
-  providers: [
-    AppService,
-    ProdutoService,
-    CategoriaService,
-    ClienteService,
-    EnderecoService,
-    PagamentoService,
-    PedidoService,
-    ItemPedidoService,
-  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
